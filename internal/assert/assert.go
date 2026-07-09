2edd6d65446345f88411dd56996d15c524ac2066
fistos3rr <fistos3rr@gmail.com>
2026-07-09 16:16:53 +0300

    tests/unit: added tests for humanDate func

---
+++ internal/assert/assert.go
@@ -0,0 +1,12 @@
+// Package assert provides helper functions for tests
+package assert
+
+import "testing"
+
+func Equal[T comparable](t *testing.T, actual, expected T) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
