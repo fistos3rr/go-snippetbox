5dfadb081b4a6d99e565a1ad52b45493564f6d7e
fistos3rr <fistos3rr@gmail.com>
2026-07-09 15:01:56 +0300

    users: authorization

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,14 +25,15 @@ func (app *application) routes() http.Handler {
 	)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	protected := dynamic.Append(app.requireAuthentication)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", 
 		dynamic.ThenFunc(app.snippetView))
 	router.Handler(http.MethodGet, "/snippet/create", 
-		dynamic.ThenFunc(app.snippetCreate))
+		protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create",
-		dynamic.ThenFunc(app.snippetCreatePost))
+		protected.ThenFunc(app.snippetCreatePost))
 
 	router.Handler(http.MethodGet, "/user/signup", 
 		dynamic.ThenFunc(app.userSignup))
@@ -43,7 +44,8 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodPost, "/user/login", 
 		dynamic.ThenFunc(app.userLoginPost))
 	router.Handler(http.MethodPost, "/user/logout", 
-		dynamic.ThenFunc(app.userLogoutPost))
+		protected.ThenFunc(app.userLogoutPost))
+
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
