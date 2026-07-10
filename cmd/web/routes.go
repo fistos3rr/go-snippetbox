098772a153e287a72ad53fd13fdc6e00a311e746
fistos3rr <fistos3rr@gmail.com>
2026-07-10 09:17:15 +0300

    tests/endtoend: test for ping endpoint

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(func(
-		w http.ResponseWriter, 
+		w http.ResponseWriter,
 		r *http.Request,
 	) {
 		app.notFound(w)
@@ -21,32 +21,33 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.FS(ui.Files))
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
+
 	dynamic := alice.New(
-		app.sessionManager.LoadAndSave, 
-		noSurf, 
+		app.sessionManager.LoadAndSave,
+		noSurf,
 		app.authenticate,
 	)
 	protected := dynamic.Append(app.requireAuthentication)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
-	router.Handler(http.MethodGet, "/snippet/view/:id", 
+	router.Handler(http.MethodGet, "/snippet/view/:id",
 		dynamic.ThenFunc(app.snippetView))
-	router.Handler(http.MethodGet, "/snippet/create", 
+	router.Handler(http.MethodGet, "/snippet/create",
 		protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create",
 		protected.ThenFunc(app.snippetCreatePost))
 
-	router.Handler(http.MethodGet, "/user/signup", 
+	router.Handler(http.MethodGet, "/user/signup",
 		dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup",
 		dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login",
 		dynamic.ThenFunc(app.userLogin))
-	router.Handler(http.MethodPost, "/user/login", 
+	router.Handler(http.MethodPost, "/user/login",
 		dynamic.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logout", 
+	router.Handler(http.MethodPost, "/user/logout",
 		protected.ThenFunc(app.userLogoutPost))
-
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
